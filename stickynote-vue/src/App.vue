ebe0d3b15047de36dc77b9151b74adab28c1cd57
unknown <bgsdewoo20@gmail.com>
2024-06-05 10:11:24 +0700

    Membuat Ui Memo Bagian Header

--- stickynote-vue/src/App.vue
+++ stickynote-vue/src/App.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    Hello World
+ <main>
+  <div class="container">
+    <header>
+      <h1 class="header-title">Memo</h1>
+      <button class="header-button">+</button>
+    </header>
+    <div>[card]</div>
   </div>
+ </main>
 </template>
+
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+
+.container {
+  max-width: 800px;
+  padding: 10px;
+  margin: 0 auto;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center
+}
+
+.header-title {
+  font-size: 48px;
+  font-weight: bold;
+  margin-bottom:25px;
+  color: #495a7d;
+}
+
+.header-button{
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  border-radius: 100%;
+  background-color: #495a7d;
+  color: white;
+}
+</style>
