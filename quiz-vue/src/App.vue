b0119309a27e9408ca3bc0293c4ca5f1eef92cdf
unknown <bgsdewoo20@gmail.com>
2024-06-05 22:39:48 +0700

    Membuat Ui Daftar Quiz

--- quiz-vue/src/App.vue
+++ quiz-vue/src/App.vue
@@ -5,13 +5,22 @@
       <h1 id="title">QuizVue</h1>
       <input type="text" id="search-input"/>
     </header>
+    <section id="quiz-container">
+      <div class="card">
+        <div class="card-body">
+          <img src="./assets/img/progamming.jpg" alt="">
+          <h2>Progamming</h2>
+          <p>2 Questions</p>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
 
 <style scoped>
 main {
-  max-width: 900px;
+  max-width: 1200px;
   margin:0 auto
 }
 
@@ -32,5 +41,34 @@ header {
   background-color:#c9c9c9d8;
   padding: 10px;
   border-radius: 5px;
+}
+
+#quiz-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 20px;
+}
+
+.card {
+  width: 1000px;
+  margin-right: 30px;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+}
+
+.card-img {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  margin: 0;
+}
+
+.card-body {
+  padding: 0 15px;
+}
+
+.card-body h2 {
+  font-weight: bold;
 }
 </style>
