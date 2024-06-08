6811e6c8e302f26b649ddb3cdada9c61125967cc
unknown <bgsdewoo20@gmail.com>
2024-06-08 21:17:31 +0700

    Ekstrak Dan Mendapatkan Data Dari Route

--- vue-router/src/views/Product.vue
+++ vue-router/src/views/Product.vue
@@ -1,5 +1,17 @@
+<script setup>
+
+import { useRoute } from 'vue-router';
+import products from "../data/products.json"
+const route = useRoute()
+const product = products.find((product) => product.id === parseInt(route.params.id))
+</script>
+
+
+
 <template>
     <div>
         <h1>Product</h1>
+      <h2>{{ product.name }} - {{ product.price }}</h2>
+        <p>Year: {{ product.year }}</p>
     </div>
 </template>
