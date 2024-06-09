092e0caa0a3621ddad57d2a31825de5dca22b498
unknown <bgsdewoo20@gmail.com>
2024-06-09 19:18:25 +0700

    Membuat Dinamis Nomor Halaman Pertanyaan

--- quiz-vue/src/views/Quiz.vue
+++ quiz-vue/src/views/Quiz.vue
@@ -1,23 +1,38 @@
 <script setup>
 import QuizHeader from '@/components/QuizHeader.vue'
 import QuizContent from '@/components/QuizContent.vue'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router';
 import quizes from '../data/quizes.json'
 
 const route = useRoute()
 const quizId = parseInt(route.params.id)
-const quiz = quizes.find((q) => q.id ===quizId)
+const quiz = quizes.find((q) => q.id === quizId)
 
 
 const currentQuestionIndex = ref(0)
+
+const questionPage = ref(
+    `${currentQuestionIndex.value  + 1} / ${quiz.questions.length}`
+)
+
+
+watch(
+    () => currentQuestionIndex.value,
+    () => {
+    questionPage.value = `${currentQuestionIndex.value + 1} / ${quiz.questions.length}`
+})
+
+
 </script>
 
 
 
 <template>
+    <QuizHeader :questionPage="questionPage"/>
     <QuizHeader />
     <QuizContent :question="quiz.questions[currentQuestionIndex]" />
+    <button @click="currentQuestionIndex++" :disabled="currentQuestionIndex === quiz.questions.length - 1">Next</button>
 </template>
 
 <style scoped>
