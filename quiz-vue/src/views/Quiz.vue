edfd3a449d5001050d9751833a91d842b27a2339
unknown <bgsdewoo20@gmail.com>
2024-06-09 19:35:00 +0700

    Mendapatkan Nilai Dari Perubahan State Dengan Computed

--- quiz-vue/src/views/Quiz.vue
+++ quiz-vue/src/views/Quiz.vue
@@ -1,7 +1,7 @@
 <script setup>
 import QuizHeader from '@/components/QuizHeader.vue'
 import QuizContent from '@/components/QuizContent.vue'
-import { ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 import { useRoute } from 'vue-router';
 import quizes from '../data/quizes.json'
 
@@ -11,16 +11,8 @@ const quiz = quizes.find((q) => q.id === quizId)
 
 
 const currentQuestionIndex = ref(0)
-
-const questionPage = ref(
-    `${currentQuestionIndex.value  + 1} / ${quiz.questions.length}`
-)
-
-
-watch(
-    () => currentQuestionIndex.value,
-    () => {
-    questionPage.value = `${currentQuestionIndex.value + 1} / ${quiz.questions.length}`
+const questionPage = computed(() => {
+    return `${currentQuestionIndex.value  + 1} / ${quiz.questions.length}`
 })
 
 
@@ -30,7 +22,6 @@ watch(
 
 <template>
     <QuizHeader :questionPage="questionPage"/>
-    <QuizHeader />
     <QuizContent :question="quiz.questions[currentQuestionIndex]" />
     <button @click="currentQuestionIndex++" :disabled="currentQuestionIndex === quiz.questions.length - 1">Next</button>
 </template>
